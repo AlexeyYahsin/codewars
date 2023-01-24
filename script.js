0458d517cf9973238891341ad877a4b9731cca9d
alexeyYashin <alexeyyashin@mail.ru>
2023-01-24 21:34:24 +0400

    l3

--- script.js
+++ script.js
@@ -35,3 +35,14 @@ function filter_list(l) {
 // console.log(filter_list([1,2,'a','b']))
 // console.log(filter_list([1,'a','b',0,15]))
 // console.log(filter_list([1,2,'aasf','1','123',123]))
+
+
+const areaOrPerimeter = function(l , w) {
+    if(l === w){
+        return l * w
+    } else {
+        return((l * 2) + (w * 2))
+    }
+  };
+areaOrPerimeter(3,  3)
+areaOrPerimeter(6, 10)
