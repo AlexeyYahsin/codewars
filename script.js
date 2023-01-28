e9619af68696b07b19ba893811130d4d465f810c
alexeyYashin <alexeyyashin@mail.ru>
2023-01-28 21:50:57 +0400

    l18

--- script.js
+++ script.js
@@ -275,7 +275,18 @@ function countSmileys(arr) {
     }
     return count;
 }
-countSmileys([])
-countSmileys([':D',':~)',';~D',':)'])
-countSmileys([':)',':(',':D',':O',':;'])
-countSmileys([';]', ':[', ';*', ':$', ';-D'])
+// countSmileys([])
+// countSmileys([':D',':~)',';~D',':)'])
+// countSmileys([':)',':(',':D',':O',':;'])
+// countSmileys([';]', ':[', ';*', ':$', ';-D'])
+
+function findUniq(arr) {
+    arr.sort((a,b)=>a-b);
+    return arr[0]==arr[1]?arr.pop():arr[0]
+  }
+findUniq([ 1, 0, 0 ])
+findUniq([ 0, 1, 0 ])
+findUniq([ 0, 0, 1 ])
+findUniq([ 1, 1, 1, 2, 1, 1 ])
+findUniq([ 1, 1, 2, 1, 1 ])
+findUniq([ 3, 10, 3, 3, 3 ])
