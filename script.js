a3eda1f92c5e52da5043c7169ab029cb5f203318
alexeyYashin <alexeyyashin@mail.ru>
2023-01-29 19:56:31 +0400

    l30

--- script.js
+++ script.js
@@ -451,6 +451,26 @@ class Kata {
 function squareSum(numbers){
     return numbers.reduce((acc, num)=> acc += Math.pow(num, 2), 0)
 }
-squareSum([1,2])
-squareSum([0, 3, 4, 5])
-squareSum([])
+// squareSum([1,2])
+// squareSum([0, 3, 4, 5])
+// squareSum([])
+
+
+function likes(names) {
+    if(names.length === 0){
+        return 'no one likes this'
+    } else if (names.length === 1){
+        return `${names[0]} likes this`
+    } else if (names.length === 2){
+        return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else{
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+}
+// likes([])
+// likes(['Peter'])
+// likes(['Jacob', 'Alex'])
+// likes(['Max', 'John', 'Mark'])
+// likes(['Alex', 'Jacob', 'Mark', 'Max'])
