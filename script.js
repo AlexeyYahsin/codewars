cc1793e227476d8e41e94f24e0f8b021bf3e150b
alexeyYashin <alexeyyashin@mail.ru>
2023-01-29 20:01:43 +0400

    l31

--- script.js
+++ script.js
@@ -474,3 +474,13 @@ function likes(names) {
 // likes(['Jacob', 'Alex'])
 // likes(['Max', 'John', 'Mark'])
 // likes(['Alex', 'Jacob', 'Mark', 'Max'])
+
+
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(''),2)
+    
+}
+// binaryArrayToNumber([0,0,0,1])
+// binaryArrayToNumber([0,0,1,0])
+// binaryArrayToNumber([1,1,1,1])
+// binaryArrayToNumber([0,1,1,0])
