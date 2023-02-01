66fd181694524b01a983be5c22ac4449062337ef
alexeyYashin <alexeyyashin@mail.ru>
2023-02-01 19:54:08 +0400

    l33

--- script.js
+++ script.js
@@ -489,5 +489,17 @@ const binaryArrayToNumber = arr => {
 function makeNegative(num) {
     return num > 0 ? num = -num : num === 0 ? 0 : num
 }
+// makeNegative(42)
 
-makeNegative(42)
+
+function sumTwoSmallestNumbers(numbers) { 
+    numbers.sort((a, b)=>{
+       return a - b
+    })
+    return numbers[0] + numbers[1]
+}  
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]))
